ce777b66db8315957b74eedd66c2a8ab54e84ff9
octoberstorm <storm.october@gmail.com>
2017-03-13 02:05:01 +0700

    support appengine urlfetch

--- client.go
+++ client.go
@@ -1,6 +1,8 @@
 package gochatwork
 
 import (
+	"appengine"
+	"appengine/urlfetch"
 	"bytes"
 	"io/ioutil"
 	"log"
@@ -66,6 +68,37 @@ func (c *Client) parseBody(resp *http.Response) []byte {
 		return []byte(``)
 	}
 	return body
+}
+
+func (c *Client) urlFetch(method, endpoint string, params map[string]string) []byte {
+	ctx := appengine.NewContext(r)
+	client := urlfetch.Client(ctx)
+	req, requestErr = http.NewRequest(method, c.buildUrl(c.BaseUrl, endpoint, params), nil)
+
+	var (
+		req        *http.Request
+		requestErr error
+	)
+
+	if method != "GET" {
+		req, requestErr = http.NewRequest(method, c.BaseUrl+endpoint, bytes.NewBufferString(c.buildBody(params).Encode()))
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	} else {
+		req, requestErr = http.NewRequest(method, c.buildUrl(c.BaseUrl, endpoint, params), nil)
+	}
+	if requestErr != nil {
+		panic(requestErr)
+	}
+
+	req.Header.Add("X-ChatWorkToken", c.ApiKey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return []byte(``)
+	}
+
+	return c.parseBody(resp)
 }
 
 func (c *Client) execute(method, endpoint string, params map[string]string) []byte {
